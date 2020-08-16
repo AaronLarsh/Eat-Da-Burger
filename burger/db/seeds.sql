25e6e073bc7311f7958f51c5aeebe600f96a2ef6
Dr Dougie <aaron.m.larsh@gmail.com>
2020-08-16 13:19:17 -0700

    testing connection.js file

---
+++ burger/db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Chicken Sammich');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheese Burger', true);
+INSERT INTO burgers (burger_name) VALUES ('Fake Burger');
