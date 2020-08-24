7650d4cd4530df6e939f01e776481bc33453c8e6
Aaron Larsh <64706584+AaronLarsh@users.noreply.github.com>
2020-08-23 21:35:30 -0700

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -9,7 +9,7 @@ if(process.env.JAWSDB_URL){
     connection = mysql.createConnection({
         host: "localhost",
         user: "root",
-        password: "MyLDA2020",
+        password: "",
         database: "burgers_db"
     });
 };
